1ab9fb62873be90c06b4dd02ce406c2816435037
Berk Burak Tasdemir <burakt546@live.com>
2021-03-11 00:16:30 -0800

    Initial commit

---
+++ parstagram-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parstagram-ios
+//
+//  Created by Berk Burak Tasdemir on 3/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
