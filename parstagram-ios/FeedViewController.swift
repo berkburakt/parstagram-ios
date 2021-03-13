1c61246a9bad07bf88a9dccd0e4179c3a6d4a166
Berk Burak Tasdemir <burakt546@live.com>
2021-03-12 21:52:38 -0800

    Pull to refresh & infinite scroll features

--- parstagram-ios/FeedViewController.swift
+++ parstagram-ios/FeedViewController.swift
@@ -12,8 +12,10 @@ import AlamofireImage
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var refreshControl = UIRefreshControl()
     
     var posts = [PFObject]()
+    var queryLimit: Int = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,20 +23,38 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.delegate = self
         tableView.dataSource = self
         
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        tableView.addSubview(refreshControl)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+        getData()
+    }
+    
+    func getData() {
         let query = PFQuery(className: "Posts")
         query.includeKey("author")
-        query.limit = 20
+        query.limit = queryLimit
         query.findObjectsInBackground { (posts, error) in
             if posts != nil {
                 self.posts = posts!
                 self.tableView.reloadData()
             }
         }
+    }
+    
+    @objc func onRefresh() {
+        getData()
+        run(after: 1) {
+            self.refreshControl.endRefreshing()
+        }
+    }
+    
+    func run(after wait: TimeInterval, closure: @escaping () -> Void) {
+        let queue = DispatchQueue.main
+        queue.asyncAfter(deadline: DispatchTime.now() + wait, execute: closure)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -52,5 +72,12 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.usernameLabel.text = user.username
         cell.commentLabel.text = post["caption"] as? String
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == posts.count {
+            queryLimit += 2
+            getData()
+        }
     }
 }
